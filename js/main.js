f49cae4dbce8a6f4199bc7a86eee89218b9b876b
Joy <putradananjoyo@gmail.com>
2017-05-12 01:28:07 +0700

    adding scrollrevealjs

---
+++ js/main.js
@@ -0,0 +1,44 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#navbar-items', {
+  duration: 1000,
+  origin: 'top'
+});
+
+sr.reveal('.jumb-1', {
+  duration: 2000,
+  origin: 'left',
+  distance: '100px'
+});
+
+sr.reveal('.jumb-2', {
+  duration: 2000,
+  origin: 'right',
+  distance: '100px'
+});
+
+sr.reveal('.sec-title', {
+  duration: 2000,
+  opacity: 0
+});
+
+sr.reveal('.item-1', {
+  duration: 2000,
+  origin: 'bottom'
+});
+
+sr.reveal('.item-2', {
+  duration: 2500,
+  origin: 'bottom'
+});
+
+sr.reveal('.item-3', {
+  duration: 3000,
+  origin: 'bottom'
+});
+
+sr.reveal('.subscribe', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '100px'
+});
